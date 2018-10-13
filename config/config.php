2dd372ce8ea9e682182101ee939e9944e33d40bd
Emmanuel <terrenemmanuel@gmail.com>
2018-10-13 17:46:24 -0300

    confgi reparado

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php namespace Config;
 
 define("ROOT", dirname(__DIR__) . "/");
-define("FRONT_ROOT", "/ISIS2/");
+define("FRONT_ROOT", "/utn/tpfinallab4/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
