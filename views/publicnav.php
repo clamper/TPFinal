65f8c12eee7f08bee5b03b1490c42c1736823a03
sergito <servega100@gmail.com>
2018-11-21 13:07:17 -0300

    login

--- views/publicnav.php
+++ views/publicnav.php
@@ -7,7 +7,7 @@
 
         <div class="btn-group navbar-right">
             <a href="/utn/TPFINALLAB4/user/viewRegistrationForm"> <button type="button" class="btn btn-info"><i class="fa fa-user-plus"></i> Registrarse</button></a>
-            <button type="button" class="btn btn-info" id="btn_login"><i class="fa fa-user"></i> Entrar</button>
+            <a href="/utn/TPFINALLAB4/user/viewLoginForm"><button type="button" class="btn btn-info" id="btn_login_old"><i class="fa fa-user"></i> Entrar</button></a>
         </div>
     </div>
 
