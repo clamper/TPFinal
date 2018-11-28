380e55e4ea215334e98ef3d354507c96895a527b
sergio <servega100@gmail.com>
2018-11-28 01:29:18 -0300

    home complete

--- views/home.php
+++ views/home.php
@@ -15,16 +15,16 @@ foreach ($showsList as $show)
 {  
 ?>
     <a href='/utn/tpfinallab4/event/showDetail/?showid=<?=$show->getIdShow()?>'>
-        <div class="card">
+        <div class="card shadow-sm ">
             <?php
                 if ( $show->getIdImage() == 0)
-                    echo "<img class='card-img-top' src='/utn/tpfinallab4/views/img/prueba_evento.svg' alt='Card image'>";
+                    echo "<img class='card-img-top ' src='/utn/tpfinallab4/views/img/prueba_evento.svg' alt='Card image'>";
                 else
                     echo "<img class='card-img-top' src='/utn/tpfinallab4/images/".$show->getIdImage()."' alt='Card image'>";
             ?>
             
             <div class="card-img-overlay">
-                <h6 class="card-title"><?=$show->getShowName()?></h6>
+                <h6 class="card-title text-black " style="background-color: rgba(245, 245, 245, 0.4);"><?=$show->getShowName()?></h6>
 
             </div>
         </div>
