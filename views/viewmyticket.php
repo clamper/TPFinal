17c62234f769144e281af19eb859f4782cba65e2
sergio <servega100@gmail.com>
2018-11-28 01:58:33 -0300

    123

--- views/viewmyticket.php
+++ views/viewmyticket.php
@@ -1,58 +1,3 @@
-<<<<<<< HEAD
-<div class="container" id="abm_container">
-        <br>
-
-        <?php
-        if ($msg != "")
-            echo "<div class='alert alert-info alert-dismissible fade show'>".
-            "<button type='button' class='close' data-dismiss='alert'>&times;</button>".$msg."</div>";
-        ?>        
-
-        <div class="card">
-            <div class="card-body">
-                <br>
-                <div class="card-text text-center">
-
-                    <table class="table text-center">
-                        <thead class="thead-dark">
-                            <tr>
-                                <th>Nro ticket</th>
-                                <th>Show</th>
-                                <th>Fecha</th>
-                                <th>Ubicacion</th>
-                                <th>Precio</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            <?php
-                            foreach ($array_ticket as $ticket) {            
-
-                                $location = $locationDAO->GetLocationById($ticket->getIdLocation());
-                                $presentation = $PresentationDAO->GetPresentationById($location->getIdPres());
-                                $seat = $SeatDAO->GetSeatbyID($location->getIdSeat());
-                                $show = $ShowDAO->GetShowByID($presentation->getIdShow());
-                            ?>
-
-                            <tr>
-                                <td><?= $ticket->getTicketNumber(); ?></td>
-                                <td><?= $show->getShowName(); ?></td>
-                                <td><?= $ticket->getDate(); ?></td>
-                                <td><?= $seat->getSeatName(); ?></td>
-                                <td style="color:green;"><?= $ticket->getPrice(); ?></td>
-                            </tr>
-
-                            <?php
-                                }
-                            ?>
-                        </tbody>
-                    </table>
-
-
-
-
-                </div>
-=======
 
 <div class="container" id="abm_container">
     <br>
@@ -106,7 +51,6 @@
 
 
 
->>>>>>> d9258c10093f2d86b4bdf4b8dab47e0197213392
             </div>
         </div>
     </div>
